611c90d08205c72663a2a0487c5d29962b98d1ca
nexus-appshub <xubilas.era@gmail.com>
2026-09-25 22:55:11 +0600

    4.7.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.homeairtv.xubilas"
     minSdk = 24
     targetSdk = 36
-    versionCode = 471
-    versionName = "4.7.1"
+    versionCode = 473
+    versionName = "4.7.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
